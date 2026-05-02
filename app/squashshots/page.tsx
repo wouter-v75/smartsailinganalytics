d8b95f0e6b1e837879ab48a0caee4ba4c69d8968
wouter-v75 <wouter@wvsailing.co.uk>
2026-05-02 18:02:38 +0200

    feat: add squash shots mobile webapp - pinch zoom, touch pan, point placement

---
+++ app/squashshots/page.tsx
@@ -0,0 +1,10 @@
+import SquashShotsApp from './SquashShotsApp';
+
+export const metadata = {
+  title: 'Squash Shots | SmartSailingAnalytics',
+  description: 'Capture and process boat photos with rotation, cropping, and squashing',
+};
+
+export default function SquashShotsPage() {
+  return <SquashShotsApp />;
+}
